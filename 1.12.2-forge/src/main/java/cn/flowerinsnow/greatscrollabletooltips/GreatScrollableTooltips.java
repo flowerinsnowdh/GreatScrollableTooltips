aa74a98dde38cc251dc259ec70aba77510382ef7
flowerinsnow <flowerimsnow@gmail.com>
2024-12-14 19:05:46 +0800

    ci(workflows): release 4.3.0+forge
    ci(workflows): add sha256sum log

--- 1.12.2-forge/src/main/java/cn/flowerinsnow/greatscrollabletooltips/GreatScrollableTooltips.java
+++ 1.12.2-forge/src/main/java/cn/flowerinsnow/greatscrollabletooltips/GreatScrollableTooltips.java
@@ -36,7 +36,7 @@ import java.nio.file.Path;
 public class GreatScrollableTooltips {
     public static final String MODID = "great-scrollable-tooltips";
     public static final String NAME = "Great Scrollable Tooltips";
-    public static final String VERSION = "4.3.0";
+    public static final String VERSION = "4.3.0+forge";
 
     private static GreatScrollableTooltips instance;
 
